cf85197bd495313282da96e60f9776e497996f00
Easierq <isiaqofficial@gmail.com>
2025-07-08 11:24:50 +0100

    new updates

--- app/dashboard/components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <nav className="fixed top-0 w-full h-16 z-[49] px-2 lg:px-4 flex justify-between items-center shadow-sm">
-      hi
-    </nav>
-  );
-};
-
-export default Navbar;
--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function MainPage() {
-  return <div className="px-4">hi</div>;
-}
